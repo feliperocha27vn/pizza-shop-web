5915d02c3984574ad619050c3c72cb49219c72a7
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-09-17 11:01:53 -0300

    feat: Página: Login

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,33 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <div>Sign In Page</div>
+      <div className="p-8 ">
+        <div className="w-[320px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input type="email" />
+            </div>
+            <Button type="submit" className="w-full cursor-pointer">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
