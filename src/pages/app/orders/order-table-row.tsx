5bdeece5d71f948882d29cca1388009a71372032
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-09-17 16:53:07 -0300

    feat: Componente: Filtros de pedidos

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+import { ArrowRight, Search, X } from 'lucide-react'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size={'xs'}>
+          <Search className="size-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-sm font-medium">#123456</TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Felipe Rocha</TableCell>
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size={'xs'}>
+          <ArrowRight className="size-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size={'xs'}>
+          <X className="size-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
