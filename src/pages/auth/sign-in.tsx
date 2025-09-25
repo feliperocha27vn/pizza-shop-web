2e1403a34e1b3cc7e457b0a188f04bcfacd4bfe7
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-09-25 11:01:12 -0300

    feat: Autenticação do usuário

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,7 +5,7 @@ import { Label } from '@radix-ui/react-label'
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -16,11 +16,17 @@ const signInFormSchema = z.object({
 type SignInForm = z.infer<typeof signInFormSchema>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
@@ -28,7 +34,9 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     try {
-      await authenticate(data)
+      await authenticate({
+        email: data.email,
+      })
 
       toast.success('Enviamos um link de acesso para seu e-mail!', {
         action: {
